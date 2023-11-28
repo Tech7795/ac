725ba5e12cd94fec2c5c99ff951a1fc5b56966da
Tech7795 <152096539+Tech7795@users.noreply.github.com>
2023-11-28 22:35:58 +0530

    Create Q7(hollow triangle).py

---
+++ Q7(hollow triangle).py
@@ -0,0 +1,14 @@
+#@title q7) Displaying pattern  hollow triangle
+
+n=int(input())
+for i in range(1,n):
+    for j in range(n-i):
+        print(" ",end="")
+    print("*",end=" ")
+    for k in range(2,i):
+        print(" ",end=" ")
+    if i>1:
+        print("*",end="")
+    print()
+for k in range(n):
+    print("*",end=" ")
