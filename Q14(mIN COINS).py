182aa7bb227a504d4f5c9e7b8ec044c78ab59ef1
Tech7795 <152096539+Tech7795@users.noreply.github.com>
2023-11-28 23:47:56 +0530

    Create Q14(mIN COINS).py

---
+++ Q14(mIN COINS).py
@@ -0,0 +1,21 @@
+#@title Q14) Min Coins to make desired value
+def min_coins(coins, target):
+    dp = [float('inf')] * (target + 1)
+    dp[0] = 0
+
+    for i in range(1, target + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[target] if dp[target] != float('inf') else 0
+
+# Example usage:
+coins = [1, 2, 5]
+target_value =11 
+
+result = min_coins(coins, target_value)
+if result :
+    print(f"Minimum coins needed for {target_value}: {result}")
+else:
+    print(f"Cannot make {target_value} with the given coins.")
